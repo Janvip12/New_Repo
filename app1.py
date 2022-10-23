f99c03c3a5c538875241713ae163d6c7c7bf77fe
Janvip12 <98436028+Janvip12@users.noreply.github.com>
2022-10-22 23:48:32 -0400

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,10 +2,10 @@ from flask import Flask, request
 import json
 import requests
 
-app = Flask(__name__)
-app.debug = True
+app1 = Flask(__name__)
+app1.debug = True
 
-@app.route('/')
+@app1.route('/')
 def studentNumber():
     dictionary =  {"Student Number" : "PUT YOUR STUDENT NUMBER HERE"}
     return json.dumps(dictionary)
